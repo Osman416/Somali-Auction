e0304e1de0202f8b00a592b3d759af52b385c055
Osman <oosman123@hotmail.com>
2025-01-05 21:28:58 -0500

    next-auth and items table

--- somali-auction/src/app/page.tsx
+++ somali-auction/src/app/page.tsx
@@ -1,30 +1,41 @@
+import { auth } from "@/auth";
+import  SignIn  from "@/components/ui/sign-in";
+import { SignOut } from "@/components/ui/sign-out";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { database } from "@/db/database"
-import { bids as bidsSchema} from "@/db/schema"
+import { database } from "@/db/database";
+import { bids as bidsSchema, items } from "@/db/schema";
 import { revalidatePath } from "next/cache";
 
 export default async function HomePage() {
-  const bids = await database.query.bids.findMany();
+  const session = await auth();
+
+  const allItems = await database.query.items.findMany();
 
   return (
-  <main className="container mx-auto">
-    <form 
-    action={async (formData: FormData) => {
-      "use server";
-      //const bid = formData.get("bid") as string;
-      await database.insert(bidsSchema).values({})
-      revalidatePath("/")
-      }}
-    >
-        <Input name="bid" placeholder="Bid" />
-        <Button type="submit">Place Bid</Button>
-    </form>
+    <main className="container mx-auto py-12">
+      {session ? <SignOut /> : <SignIn />}
 
-    {bids.map((bid) => (
-      <div key={bid.id}>{bid.id}</div>
-    ))}
+      {session?.user?.name}
 
-  </main>
+      <form
+        action={async (formData: FormData) => {
+          "use server";
+          // const bid = formData.get("bid") as string;
+          await database.insert(items).values({
+            name: formData.get("name") as string,
+            userId: session?.user?.id!,
+          });
+          revalidatePath("/");
+        }}
+      >
+        <Input name="name" placeholder="Name your item" />
+        <Button type="submit">Post Item</Button>
+      </form>
+
+      {allItems.map((item) => (
+        <div key={item.id}>{item.name}</div>
+      ))}
+    </main>
   );
 }
